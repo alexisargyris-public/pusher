8af5207b719fbddbf00e070697a9ee78c80b6563
Alexis Argyris <alexis.argyris@gmail.com>
2018-04-23 13:40:59 +0300

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,36 @@
 
 import * as vscode from 'vscode'
 
+/**
+ * This is called by vscode to activate the command contributed by the extension
+ * @param context
+ */
 export function activate(context: vscode.ExtensionContext) {
-  // execute once when the command is activated
   console.log('pusher activated')
+  // exit immediately if no document is open
+  if (!vscode.workspace.textDocuments.length) {
+    return
+  }
+
+  // TODO note which editor currently has focus
+
+  // change event handler
+  vscode.workspace.onDidChangeTextDocument(
+    (ev: vscode.TextDocumentChangeEvent) => {
+      // TODO ignore change events of different editors
+
+      if (
+        vscode.window.activeTextEditor &&
+        ev.document === vscode.window.activeTextEditor.document
+      ) {
+        console.log(ev.contentChanges[0].text)
+      }
+    }
+  )
+
+  // the command associated with the extension
   let disposable = vscode.commands.registerCommand('extension.pusher', () => {
     // execute every time the command is executed
-    console.log('pusher running')
   })
   context.subscriptions.push(disposable)
 }
