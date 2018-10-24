286968cfdb71345b4c3cbc1557f3d76aaf361462
Alexis Argyris <alexis.argyris@gmail.com>
2018-10-24 16:44:55 +0300

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -32,97 +32,61 @@ class Queue {
 class FlashableQueue extends Queue {
   private sessionId: string
   private db: Database
+  isFlashing: boolean = false
   constructor(sessionId: string, db: Database) {
     super()
     this.sessionId = sessionId
     this.db = db
   }
+  putContents(contents) {
+    this.contents = contents
+  }
   flash() {
-    return this.db
-      .createEvent(makeTimestamp(), this.sessionId, this.contents)
-      .then(() => {})
-      .catch(error => {
+    this.isFlashing = true
+    return this.db.createEvent(makeTimestamp(), this.sessionId, this.contents).then(() => {
+      this.isFlashing = false
+      this.empty()
+    })
+  }
+}
+class QueueController {
+  private mainQueue: FlashableQueue
+  private spareQueue: Queue
+  private readonly queueSizeLimit: any = 1000 // FIXME: should be of type number
+  constructor(sessionId: string, db: Database) {
+    this.mainQueue = new FlashableQueue(sessionId, db)
+    this.spareQueue = new Queue()
+  }
+  async add(editorEvent) {
+    if (this.mainQueue.getContentsSize > this.queueSizeLimit) {
+      // main queue is full, start flashing and meanwhile use spare queue
+      try {
+        // check if the main queue is already being flashed
+        if (!this.mainQueue.isFlashing) {
+          let fl = await this.mainQueue.flash()
+          let spareContent = this.spareQueue.getContents()
+          // check if there are any spare events waiting to be processed
+          if (spareContent.length > 0) {
+            this.mainQueue.putContents(spareContent)
+            this.spareQueue.empty()
+          }
+        }
+        // store event in spare queue
+        this.spareQueue.add(editorEvent)
+      } catch(error) {
         // something went wrong
         let errorMsg = `[ERROR] Pusher.eventLoop caused: ${error.message}`
         console.error(errorMsg)
         vscode.window.showErrorMessage(errorMsg)
         deactivate()
-      })
-  }
-}
-class queueController {
-  private mainQueue: FlashableQueue
-  private spareQueue: Queue
-  private readonly mainSizeLimit: any = 1000
-  constructor(sessionId: string, db: Database) {
-    this.mainQueue = new FlashableQueue(sessionId, db)
-    this.spareQueue = new Queue()
-  }
-  add(editorEvent) {
-    if (this.mainQueue.getContentsSize > this.mainSizeLimit) {
-      this.spareQueue.add(editorEvent)
+      }
     } else {
+      // main queue still has space, use it
       this.mainQueue.add(editorEvent)
     }
   }
 }
 
-/**
- * EventQueue provides functionality related to event queue management
- *
- * how to calculate the size of a js object:
- * https://stackoverflow.com/questions/1248302/how-to-get-the-size-of-a-javascript-object
- * aws dynamodb limits:
- * https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Limits.html
- */
-class EventQueue {
-  private mainQueue: any[] = []
-  private spareQueue: any[] = [] // to store events while the contents of the main queue are being processed
-  private isMainQueueBusy: boolean = false
-  private sizeOfMainQueue: number = 0
-  readonly sizeQueueLimit = 100000
-  constructor() {}
-  private calcSize(object): number {
-    let objectList = []
-    let stack = [object]
-    let bytes = 0
-
-    while (stack.length) {
-      let value = stack.pop()
-
-      if (typeof value === 'boolean') {
-        bytes += 4
-      } else if (typeof value === 'string') {
-        bytes += value.length * 2
-      } else if (typeof value === 'number') {
-        bytes += 8
-      } else if (
-        typeof value === 'object' &&
-        objectList.indexOf(value) === -1
-      ) {
-        objectList.push(value)
-
-        for (var i in value) {
-          stack.push(value[i])
-        }
-      }
-    }
-    return bytes
-  }
-  private flushQueue() {}
-  addEvent(ev: any) {
-    // decide to which queue to add the event
-    if (this.isMainQueueBusy) {
-      this.spareQueue.push(ev)
-    } else {
-    }
-    // calculate the size of the current event
-    // update the event queue size total
-    // decide if the main queue is large enough to flush
-  }
-}
-
-let eq = new EventQueue()
 let statusBarItem: vscode.StatusBarItem
 
 /**
@@ -130,17 +94,11 @@ let statusBarItem: vscode.StatusBarItem
  * @param context
  */
 export async function activate(context: vscode.ExtensionContext) {
-  const eventLoopTimeoutLimit = 4000 // how often does the event loop get called
-  const eventQueueProcessLimit = 19 // actually the limit is 25, see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
   const extBarLabel = 'Pusher: '
   let fileId: any // Promise<String | Boolean>
   let sessionId: any // Promise<String>
-  let eventQueue: any[] = []
-  let eventQueueSpare: any[] = []
-  let isEventLoopBusy: boolean = false
-  let eventsBeingProcessed: number
-  const spareQueue: Queue = new Queue()
-  let mainQueue: Queue
+  let db: Database
+  let qc: QueueController
 
   function createFirstChange(doc) {
     return {
@@ -177,128 +135,27 @@ export async function activate(context: vscode.ExtensionContext) {
       })
     })
   }
-  function eventLoop() {
-    function processEventsBatch(): Promise<any[]> {
-      return new Promise((resolve, reject) => {
-        let events = []
-        // create the event records
-        for (let index = 0; index < eventsBeingProcessed; index++) {
-          events.push({
-            eventId: eventQueue[index].timestamp.toString(),
-            sessionId: sessionId,
-            content: eventQueue[index].contentChanges[0]
-          })
-        }
-        return db
-          .batchCreateEvent(events)
-          .then(res => {
-            // remove processed items from queue
-            for (let index = 0; index < eventsBeingProcessed; index++) {
-              eventQueue.shift()
-            }
-            resolve(res)
-          })
-          .catch(err => {
-            reject(err)
-          })
-      })
-    }
-    // update status
-    statusBarItem.text = extBarLabel + eventQueue.length
-    statusBarItem.show()
-    if (isReadyToFlush(eventQueue)) {
-      isEventLoopBusy = true
-      flush(eventQueue)
-    } else {
-      // reset the timeout
-      timerId = setTimeout(eventLoop, eventLoopTimeoutLimit)
-      isEventLoopBusy = false
-    }
-
-    /*
-    // check if there are any events to process
-    if (eventQueue.length) {
-      // there are events to process
-      isEventLoopBusy = true
-      // set events to process: min of eventQueueProcessLimit, eventQueue.length
-      eventsBeingProcessed =
-        eventQueueProcessLimit < eventQueue.length
-          ? eventQueueProcessLimit
-          : eventQueue.length
-      processEventsBatch()
-        .then(res => {
-          // check if we received as many events as we've sent
-          if (res.length === eventsBeingProcessed) {
-            // all ok
-            // check if the spare event queue contains any events
-            if (eventQueueSpare.length) {
-              console.log(
-                `found ${eventQueueSpare.length} events in spare queue`
-              )
-              // copy the events from the spare to the main event queue
-              for (let index = 0; index < eventQueueSpare.length; index++) {
-                eventQueue.push(
-                  JSON.parse(JSON.stringify(eventQueueSpare[index]))
-                )
-              }
-              // empty spare queue
-              eventQueueSpare.length = 0
-            }
-            // reset the timeout
-            timerId = setTimeout(eventLoop, eventLoopTimeoutLimit)
-            isEventLoopBusy = false
-          } else {
-            // something went wrong
-            let errorMsg = `[ERROR] Pusher.eventLoop: ${eventsBeingProcessed} events were sent, but only ${
-              res.length
-            } were received, so deactivating extension`
-            console.error(errorMsg)
-            vscode.window.showErrorMessage(errorMsg)
-            deactivate()
-          }
-        })
-        .catch(err => {
-          // top error handler
-          // TODO: partial batch success / failure
-          console.error(err)
-          vscode.window.showErrorMessage(err.message)
-          deactivate()
-        })
-    } else {
-      // no events to process; reset the timeout
-      timerId = setTimeout(eventLoop, eventLoopTimeoutLimit)
-    }
-*/
-  }
   function onTextChange(event: any) {
     // the text in the active editor was changed; store the change
     if (event.contentChanges.length) {
       event.timestamp = makeTimestamp()
-      eq.addEvent(event)
-      // // check which event queue to use
-      // if (isEventLoopBusy) {
-      //   console.log('main loop is busy')
-      //   eventQueueSpare.push(event)
-      // } else {
-      //   eventQueue.push(event)
-      // }
+      this.qc.addEvent(event)
     }
   }
 
   // if no document is open, exit immediately
   if (typeof vscode.window.activeTextEditor === 'undefined') return
   // init appsync db and first event (whole document)
-  const db = new Database()
   try {
     fileId = await findOrCreateFile(
       vscode.window.activeTextEditor.document.fileName
     )
     sessionId = await createSession(fileId)
-    mainQueue = new FlashableQueue(sessionId, db, spareQueue)
-    mainQueue.add(createFirstChange(vscode.window.activeTextEditor.document))
-    // eventQueue.push(createFirstChange(vscode.window.activeTextEditor.document))
+    db = new Database()
+    qc = new QueueController(sessionId, db)
+    qc.add(createFirstChange(vscode.window.activeTextEditor.document))
     // change text content event handler
-    // TODO: event should be vscode.TextDocumentChangeEvent
+    // FIXME: event should be vscode.TextDocumentChangeEvent
     vscode.workspace.onDidChangeTextDocument((event: any) => {
       onTextChange(event)
     })
@@ -307,8 +164,6 @@ export async function activate(context: vscode.ExtensionContext) {
       // the active editor was changed; hide and deactivate the extension
       deactivate()
     })
-    // the event processing loop
-    // timerId = setTimeout(eventLoop, eventLoopTimeoutLimit)
     // the command associated with the extension
     let disposable = vscode.commands.registerCommand('extension.pusher', () => {
       // all necessary set up work has already been done (on first activation); nothing to do here (i.e. on each subsequent activation)
