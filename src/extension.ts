75edc5256c4ff802c3084f477e287caf443eb6ea
Alexis Argyris <alexis.argyris@gmail.com>
2019-03-15 16:35:12 +0200

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ let sbi: vscode.StatusBarItem
  * This is called by vscode to activate the command contributed by the extension
  * @param context
  */
-export async function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
   const extensionName: string = 'extension.pusher'
   const sbiCommandId: string = '_anmzDeactivate'
   const sbiTextWhileLoading: string = 'Pusher: loading...'
@@ -33,14 +33,14 @@ export async function activate(context: vscode.ExtensionContext) {
   function onTextChange(
     event: vscode.TextDocumentContentChangeEvent,
     qc: QueueController
-  ) {
+  ): void {
     // the text in the active editor was changed; store the change
     qc.add({
       timestamp: makeTimestamp(),
       contentChanges: event
     })
   }
-  async function onSbiClick() {
+  async function onSbiClick(): Promise<void> {
     try {
       qc.mergeQueues()
       await qc.flash()
@@ -50,14 +50,33 @@ export async function activate(context: vscode.ExtensionContext) {
       vscode.window.showErrorMessage(errorMsg)
     }
   }
+  async function start(): Promise<void> {
+    // init status bar item
+    sbi = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, sbiPriority)
+    sbi.tooltip = sbiTooltip
+    sbi.text = sbiTextWhileLoading
+    sbi.command = sbiCommandId
+    sbi.show()
+    // init queue controller
+    qc = new QueueController(context, sbi)
+    try {
+      await qc.init()
+      qc.add(createFirstChange(vscode.window.activeTextEditor.document))
+    } catch (error) {
+      // something went wrong; deactivate the extension
+      console.error(`[ERROR] Pusher.activate caused: ${error.message}`)
+      deactivate()
+    }
+  }
+  function stop(): void {
+    deactivate()
+  }
 
   // if no document is open, exit immediately
   if (typeof vscode.window.activeTextEditor === 'undefined') return
   // extension activation command handler
-  context.subscriptions.push(vscode.commands.registerCommand(extensionName, () => {
-    // the command associated with the extension
-    // all init work has been done on first activation; here should go any re-activation work
-  }))
+  context.subscriptions.push(vscode.commands.registerCommand(extensionName + 'Start', () => start()))
+  context.subscriptions.push(vscode.commands.registerCommand(extensionName + 'Stop', () => stop()))
   // status bar item click command handler
   context.subscriptions.push(vscode.commands.registerTextEditorCommand(sbiCommandId, onSbiClick))
   // change active editor event handler
@@ -74,28 +93,12 @@ export async function activate(context: vscode.ExtensionContext) {
       }
     }
   ))
-  // init status bar item
-  sbi = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, sbiPriority)
-  sbi.tooltip = sbiTooltip
-  sbi.text = sbiTextWhileLoading
-  sbi.command = sbiCommandId
-  sbi.show()
-  // init queue controller
-  qc = new QueueController(context, sbi)
-  try {
-    await qc.init()
-    qc.add(createFirstChange(vscode.window.activeTextEditor.document))
-  } catch (error) {
-    // something went wrong; deactivate the extension
-    console.error(`[ERROR] Pusher.activate caused: ${error.message}`)
-    deactivate()
-  }
 }
 
 /**
  * This is called by vscode when the extension needs to be deactivated
  */
-export async function deactivate() {
+export async function deactivate(): Promise<void> {
   try {
     qc.mergeQueues()
     await qc.flash()
